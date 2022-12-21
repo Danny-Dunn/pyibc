42ef42ff0867d11a54ca7b949d6810ed152a53fe
Danny-Dunn <49325378+Danny-Dunn@users.noreply.github.com>
2022-12-20 23:31:10 -0500

    add sensor query

--- pyibc/ibcboiler.py
+++ pyibc/ibcboiler.py
@@ -1,8 +1,10 @@
 import http.client
 import json
+import urllib
 
 bc2_objects = {
     "network":      34,
+    "sensors":      20,
     "status":       19,
     "modelinfo":    11,
     "loads":        13,
@@ -11,28 +13,58 @@ bc2_objects = {
     "load-extended":  16
 }
 
+class LoadStatus:
+    __init__():
+        inletTemp = 0.0
+        outletTemp = 0.0
+        targetTemp = 0.0
+        firingRate = 0.0 #kW
+        isFiring = False
+
+class BoilerStatus:
+    __init__():
+        isFiring = False
+        firingRate = 0.0 #kW
+        loads = [
+            LoadStatus(),
+            LoadStatus(),
+            LoadStatus(),
+            LoadStatus(),
+        ]
+
+
+
+
 class boiler:
     ip_addr = ""
     boiler_no = 0
+    num_loads = 0
+
+
 
     def __init__(self, ip_addr, boiler_no = 0):
         self.ip_addr = ip_addr
         self.boiler_no = boiler_no
         self.conn = http.client.HTTPConnection(ip_addr)
+        num_loads = self.get_num_loads()
 
-    def get_object(self, objectid):
+    def celsius_from_raw(raw):
+        return raw/4
+
+    def query_object(self, objectid):
         query = {
             "object_no": 100,
             "object_request": bc2_objects[objectid],
             "boiler_no": self.boiler_no
         }
-        self.conn.request("GET", "/cgi-bin/bc2-cgi?json=" + json.dumps(query), headers = {"cookie" : "MetricMode=0; TECH_AUTHORIZED=103"})
+        self.conn.request("GET", "/cgi-bin/bc2-cgi?json=" + urllib.parse.quote_plus(json.dumps(query)), headers = {"cookie" : "MetricMode=0; TECH_AUTHORIZED=103"})
         response = self.conn.getresponse()
+        print(urllib.parse.quote_plus("/cgi-bin/bc2-cgi?json=" + json.dumps(query)))
         print(response.reason)
         print(response.status)
         return json.loads(response.read())
 
-    def get_load(self, load_no):
+    def query_load(self, load_no):
         query = {
             "object_no": 100,
             "object_request": bc2_objects["load"],
@@ -41,10 +73,9 @@ class boiler:
         }
         self.conn.request("GET", "/cgi-bin/bc2-cgi?json=" + json.dumps(query))
         response = self.conn.getresponse()
-        print(response.reason)
-        print(response.status)
+        
     
-    def get_load_extended(self, load_no):
+    def query_load_extended(self, load_no):
         query = {
             "object_no": 100,
             "object_request": bc2_objects["load-extended"],
@@ -53,15 +84,18 @@ class boiler:
         }
         self.conn.request("GET", "/cgi-bin/bc2-cgi?json=" + json.dumps(query))
         response = self.conn.getresponse()
-        print(response.reason)
-        print(response.status)
+        if response.status != 200:
+            return 
+
+    
 
     def get_num_loads(self):
-        loads = self.get_object("loads")
+        loads = self.query_object("loads")
         numloads = 0
         for i in range(1,5):
-            if loads["Load" + i + "Type"] != 0:
+            if loads["Load" + str(i) + "Type"] != 0:
                 numloads += 1
+        return numloads
 
     def check_connection(self):
-        self.get_object("network")
+        self.query_object("network")
